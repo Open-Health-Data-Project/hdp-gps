8e683920c3661720097d9d34ca2633286b646a04
KrzysiekDev <krzysztof.michalski1999@gmail.com>
2020-07-12 14:02:26 +0200

    Initial project structure

---
+++ hdp-gps/clean.py
@@ -0,0 +1,8 @@
+# Team 3
+defaults_thresholds = {}
+
+default_max_time = {}
+
+
+def clean_gps(dataframes: list, min_speed: float, max_time: float):
+    pass
---
+++ hdp-gps/srtm.py
@@ -0,0 +1,3 @@
+# Team 3
+def get_real_altitude(dataframes: list):
+    pass
